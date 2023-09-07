5b88549e2387edda5f60c727e84c1acf716559e7
Yaro <yaroslav.naumchak@movadex.com>
2023-09-08 00:11:32 +0300

    init

---
+++ src/LightMode.cpp
@@ -0,0 +1,10 @@
+class LightMode {
+public:
+   LightMode(int start, int end) {
+        startTime = start;
+        endTime = end;
+    }
+    public:
+        int startTime;
+        int endTime;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,13 @@
 #include <Wire.h>
 #include <WiFi.h>
 #include "time.h"
+#include "RTClib.h"
+#include "LightMode.cpp"
 
 
-BH1750 lightMeter;
+BH1750 lightMeter(0x23);
+RTC_DS3231 rtc;
+
 
 /*PIN OUT CONFIG*/
 #define LED_PIN 18
@@ -18,9 +22,10 @@ const char* password = "33333333";
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 0;
 const int   daylightOffset_sec = 3600;
-int hours = 0;
-int minutes = 0;
 CRGB leds[NUM_LEDS];
+LightMode lightMode1(23, 0);  
+LightMode lightMode2(0, 7);
+// LightMode lightMode1(11, 12);  
 
 /*FUNCTIONS DECLARATION*/
 void initWiFi();
@@ -31,9 +36,13 @@ void setup() {
   Wire.begin();
   lightMeter.begin();
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  // initWiFi();
-  Serial.print("RRSI: ");
-  Serial.println(WiFi.RSSI());
+  if (! rtc.begin()) {
+    Serial.println("Could not find RTC! Check circuit.");
+    while (1);
+  }
+  rtc.adjust(DateTime(__DATE__, __TIME__));
+  // Serial.print("RRSI: ");
+  // Serial.println(WiFi.RSSI());
 }
 
 void initWiFi() {
@@ -49,24 +58,43 @@ void initWiFi() {
 }
 
 void loop() {
+  
+  DateTime now = rtc.now();
   float lux_original = lightMeter.readLightLevel();
   int lux_prepared = (int)round(lux_original);
-
   if(lux_prepared < MINIMAL_DAYLIGHT_BRIGHTNESS) {
 
     int map_lux_to_rgb_val = map(lux_prepared, MINIMAL_DAYLIGHT_BRIGHTNESS, 0, 1, 255);
     for (int i = 0; i < NUM_LEDS; i++) {
       leds[i] = CRGB(255, random(50, 111), 1);
     }
-
-
-    Serial.println(map_lux_to_rgb_val);
-    FastLED.setBrightness(map_lux_to_rgb_val);
+    if(now.hour() >= lightMode1.startTime && now.hour() != lightMode1.endTime) {
+      if(now.hour() >= lightMode1.startTime && now.hour() != lightMode1.endTime) {
+        Serial.println(now.minute() * 60 + now.second());
+        int seconds_to_brightnes = map(now.minute() * 60 + now.second(), 0, 3600, 255, 0);
+        Serial.println(seconds_to_brightnes);
+        FastLED.setBrightness(seconds_to_brightnes);
+        Serial.println(seconds_to_brightnes);
+      }
+      if(now.hour() >= lightMode2.startTime && now.hour() <= lightMode2.endTime) {
+        clear();
+      }
+    } else  {
+      FastLED.setBrightness(map_lux_to_rgb_val);
+    }
     FastLED.show();
   } else {
     clear();
   }
-  delay(100);
+
+  Serial.print(now.hour());
+  Serial.print(":");
+  Serial.print(now.minute());
+  Serial.print(":");
+  Serial.println(now.second());
+
+
+  delay(2000);
 }
 void clear() {
   Serial.println("clean up");
